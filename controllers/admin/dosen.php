3716ee1bfc0be3bcd688fe937e11d987cf6adaf2
King-Jamal <ikadekrastika@gmail.com>
2025-06-25 23:22:02 +0800

    fix : (crud dosen) admin

--- controllers/admin/dosen.php
+++ controllers/admin/dosen.php
@@ -14,12 +14,13 @@ if(isset($_POST['submit'])) {
 
 // Update data
 if(isset($_POST['update'])){
+	$id = $_POST['id'];
 	$NIP = $_POST['NIP'];
 	$nama = $_POST['Nama'];
 	$username = $_POST['Username'];
 	$password = $_POST['Password'];
 
-	$koneksi->query("UPDATE mahasiswa SET NIP='$NIP', Nama='$nama', Username='$username', Password='$password' WHERE Dosen_id='$id'");
+	$koneksi->query("UPDATE dosen SET NIP='$NIP', Nama='$nama', Username='$username', Password='$password' WHERE Dosen_id='$id'");
 	header("location: dosen.php");
 }
 // Edit data
@@ -48,8 +49,24 @@ if(isset($_GET['hapus'])){
  
 	<h2>Tabel Dosen</h2>
 	<br/>
-	<a href="tambah.php">+ Tambah Dosen</a>
 	<br/>
+	<h3><?= $edit ? "Edit Data" : "Tambah Data" ?></h3>
+	<form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+		<input type="hidden" name="id" value="<?= $edit ? $data['Dosen_id'] : "" ?>">
+		NIP :<input type="text" name="NIP" value="<?= $edit ? $data['NIP'] : "" ?>" required>
+		<br>
+		Nama : <input type="text" name="Nama" value="<?= $edit ? $data['Nama'] : "" ?>" required>
+		<br>
+		Username :<input type="text" name="Username" value="<?= $edit ? $data['Username'] : "" ?>" required>
+		<br>
+		Password :<input type="text" name="Password" value="<?= $edit ? $data['Password'] : "" ?>" required>
+		<br>
+		<button type="submit" name="<?= $edit ? "update" : "submit" ?>"><?= $edit ? "Update" : "Tambahkan" ?></button>
+		<?php if($edit): ?>
+			<a href="dosen.php">Batal</a>
+		<?php endif; ?>
+
+	</form>
 	<br/>
 	<table border="1">
 		<tr>
@@ -62,19 +79,18 @@ if(isset($_GET['hapus'])){
 		</tr>
 		<?php 
 		include '../../config/database.php';
-		$no = 1;
-		$data = mysqli_query($koneksi,"select * from dosen");
+		$data = mysqli_query($koneksi,"SELECT * FROM dosen");
 		while($d = mysqli_fetch_array($data)){
 			?>
 			<tr>
-				<td><?php echo $no++ ?></td>
+				<td><?php echo $d['Dosen_id']; ?></td>
 				<td><?php echo $d['NIP']; ?></td>
 				<td><?php echo $d['Nama']; ?></td>
 				<td><?php echo $d['Username']; ?></td>
 				<td><?php echo $d['Password']; ?></td>
 				<td>
-                   <a href=>EDIT</a>
-					<a href=>HAPUS</a>
+					<a href="<?= $_SERVER['PHP_SELF'] . '?edit=' . $d['Dosen_id'] ?>">EDIT</a>
+					<a href="<?= $_SERVER['PHP_SELF'] . '?hapus=' . $d['Dosen_id'] ?>">HAPUS</a>
 				</td>
 			</tr>
 			<?php 
